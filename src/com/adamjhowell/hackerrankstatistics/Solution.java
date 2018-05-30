2824b667b1ea29dca278f96e80693c237f3c56c3
AdamJHowell <ajh14113>
2018-05-30 16:27:17 -0600

    About to convert to a format that will just paste directly into HackerRank.

---
+++ src/com/adamjhowell/hackerrankstatistics/Solution.java
@@ -0,0 +1,22 @@
+package com.adamjhowell.hackerrankstatistics;
+
+
+import java.util.Scanner;
+
+
+public class Solution
+{
+	@SuppressWarnings( "squid:S106" )
+	public static void main( String[] args )
+	{
+		Scanner in = new Scanner( System.in );
+		int q = in.nextInt();
+		for( int i = 0; i < q; i++ )
+		{
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int n = in.nextInt();
+		}
+		in.close();
+	}
+}
